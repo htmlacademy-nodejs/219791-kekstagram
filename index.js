e5e597996a3066e6ad30687f21c52dc9830a4e6c
Grigoriy Andrievskiy <andrievskiy.g@yandex.ru>
2018-09-26 23:53:37 +0300

    fix

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-let commandList = {
-	'--version': 'v0.0.1',
+const commandList = {
+	'--version': 'v0.0.2',
 	'--help': 'Доступные команды:\n--help    —view help;\n--version —view app version;',
 };
-let command = process.argv[2];
 
+const errorMessage = msg => `Неизвестная команда ${msg}. Чтобы прочитать правила использования приложения, наберите "--help"`;
+const command = process.argv[2];
 
-if (command) {
-	if (commandList[command]) {
-		console.log(commandList[command]);
-		process.exit(0);
-	} else {
-		console.error(`Неизвестная команда ${command}. Чтобы прочитать правила использования приложения, наберите "--help"`);
-		process.exit(1);
-	}
-} else  {
-	console.log('Привет пользователь!\nЭта программа будет запускать сервер "Kekstagram".\nАвтор: Grigoriy Andrievskiy.');
-	process.exit(0);
+let responseMessage, responseCode = 0, response = console.log;
+
+if (!command) {
+  responseMessage = 'Привет пользователь!\nЭта программа будет запускать сервер "Kekstagram".\nАвтор: Grigoriy Andrievskiy.';
+} else if (commandList[command]) {
+  responseMessage = commandList[command];
+} else {
+  response = console.error;
+  responseMessage = errorMessage(command);
+  responseCode = 1;
 }
+
+response(responseMessage);
+process.exit(responseCode);
