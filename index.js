d63133c0517c6eb9ba523236f17f83fa739fa4cd
Grigoriy Andrievskiy <andrievskiy.g@yandex.ru>
2018-09-27 22:44:12 +0300

    module2-task1

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+'use strict';
+
 const commandList = {
-	'--version': 'v0.0.2',
-	'--help': 'Доступные команды:\n--help    —view help;\n--version —view app version;',
+  '--version': `v0.0.2`,
+  '--help': `Доступные команды:\n--help    —view help;\n--version —view app version;`,
 };
 
-const errorMessage = msg => `Неизвестная команда ${msg}. Чтобы прочитать правила использования приложения, наберите "--help"`;
+const errorMessage = (msg) => `Неизвестная команда ${msg}. Чтобы прочитать правила использования приложения, наберите "--help"`;
 const command = process.argv[2];
 
 if (!command) {
-  console.log('Привет пользователь!\nЭта программа будет запускать сервер "Kekstagram".\nАвтор: Grigoriy Andrievskiy.');
+  console.log(`Привет пользователь!\nЭта программа будет запускать сервер "Kekstagram".\nАвтор: Grigoriy Andrievskiy.`);
   process.exit(0);
 } else if (commandList[command]) {
   console.log(commandList[command]);
