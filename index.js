02b16c079a7a686dc7bb7e3afcfc634b3f699753
Grigoriy Andrievskiy <andrievskiy.g@yandex.ru>
2018-10-02 20:21:13 +0300

    fix

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const license = require(`./commands/license.js`);
 const description = require(`./commands/description.js`);
 
 const commandList = {version, help, license, description};
-const command = process.argv[2];
+const command = process.argv[2].slice(2);
 
 if (!command) {
   empty.execute();
   process.exit(0);
-} else if (commandList[command.substring(2)]) {
-  commandList[command.substring(2)].execute();
+} else if (commandList[command] && `--${command}` === process.argv[2]) {
+  commandList[command].execute();
   process.exit(0);
 } else {
-  error.execute(command);
+  error.execute(process.argv[2]);
   process.exit(1);
 }
