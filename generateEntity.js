331248217527d4b63f5ffcfddf34f97b5bfa4e65
Grigoriy Andrievskiy <andrievskiy.g@yandex.ru>
2018-10-04 14:12:33 +0300

    module3-task2

---
+++ generateEntity.js
@@ -0,0 +1,65 @@
+'use strict';
+
+module.exports = () => {
+  const MIN_TAG_LENGTH = 2;
+  const MAX_TAG_LENGTH = 20;
+  const MAX_TAGS_NUMBER = 5;
+
+  const MIN_WORD_LENGTH = 3;
+  const MAX_WORD_LENGTH = 20;
+
+  const MAX_TEXT_LENGTH = 140;
+
+  const MAX_COMMENTS_NUMBER = 10;
+
+  const possibleEffects = [`none`, `chrome`, `sepia`, `marvin`, `phobos`, `heat`];
+  const getRandom = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
+  const getRandomChar = () => String.fromCharCode(getRandom(97, 122));
+  const getRandomWord = (min, max) => {
+    let word = ``;
+    for (let i = min; i <= getRandom(min, max); i++) {
+      word += getRandomChar();
+    }
+    return word;
+  };
+
+  const getRandomTags = () => {
+    let tags = [];
+    for (let i = 0; i < MAX_TAGS_NUMBER; i++) {
+      tags.push(`#` + getRandomWord(MIN_TAG_LENGTH, MAX_TAG_LENGTH - 1));
+    }
+    return tags;
+  };
+
+  const getRandomDescription = () => {
+    let maxLength = getRandom(0, MAX_TEXT_LENGTH);
+    let description = ``;
+    let word = ` `;
+    while ((description.length + word.length) <= maxLength) {
+      description += word;
+      word = ` ` + getRandomWord(MIN_WORD_LENGTH, MAX_WORD_LENGTH);
+    }
+    return description;
+  };
+
+  const getRandomComments = () => {
+    let comments = [];
+    for (let i = 0; i < getRandom(0, MAX_COMMENTS_NUMBER); i++) {
+      comments.push(getRandomDescription());
+    }
+    return comments;
+  }
+
+
+  const week = 7 * 24 * 60 * 60 * 1000;
+  return {
+    url: `https://picsum.photos/600/?random`,
+    scale: getRandom(0, 100),
+    effect: possibleEffects[getRandom(0, possibleEffects.length - 1)],
+    hashtags: getRandomTags(),
+    description: getRandomDescription(),
+    likes: getRandom(0, 1000),
+    comments: getRandomComments(),
+    date: Date.now() - getRandom(0, week)
+  };
+};
---
+++ test/generator.test.js
@@ -0,0 +1,61 @@
+`use strict`;
+
+const assert = require(`assert`);
+const generateEntity = require(`../generateEntity.js`);
+
+describe(`generateEntity`, () => {
+  const entitiy = generateEntity();
+  console.log(`entitiy`, entitiy);
+
+  it(`Has valid url`, () => {
+    assert(entitiy.hasOwnProperty(`url`));
+    assert.equal(entitiy.url, `https://picsum.photos/600/?random`);
+  });
+
+  it (`Has valid scale`, () => {
+    assert(entitiy.hasOwnProperty(`scale`));
+    assert((typeof entitiy.scale) === `number` && entitiy.scale >= 0 && entitiy.scale <= 100);
+  });
+
+  it (`Has valid effect type`, () => {
+    assert(entitiy.hasOwnProperty(`effect`));
+    assert([`none`, `chrome`, `sepia`, `marvin`, `phobos`, `heat`].includes(entitiy.effect));
+  });
+
+ it (`Has valid hashtags`, () => {
+    assert(entitiy.hasOwnProperty(`hashtags`));
+    assert(Array.isArray(entitiy.hashtags));
+    assert(entitiy.hashtags.length > 0 && entitiy.hashtags.length <= 5);
+    assert(entitiy.hashtags.every((tag) => (typeof tag) === `string` && tag[0] === `#` && tag.length <= 20 && !tag.includes(` `)));
+    const tags = entitiy.hashtags.reduce((accum, tag) => {
+      if (accum.indexOf(tag.slice(1).toLowerCase()) === -1) {
+        accum.push(tag.slice(1).toLowerCase());
+      }
+      return accum;
+    }, []);
+    assert(tags.length === entitiy.hashtags.length);
+  });
+
+  it (`Has valid description`, () => {
+    assert(entitiy.hasOwnProperty(`description`));
+    assert((typeof entitiy.description) === `string` && entitiy.description.length <= 140);
+  });
+
+  it (`Has valid likes`, () => {
+    assert(entitiy.hasOwnProperty(`likes`));
+    assert((typeof entitiy.likes) === `number` && entitiy.likes >= 0 && entitiy.likes <= 1000);
+  });
+
+  it (`Has valid comments`, () => {
+    assert(entitiy.hasOwnProperty(`comments`));
+    assert(Array.isArray(entitiy.comments));
+    assert(entitiy.comments.every((comment) => (typeof comment) === `string` && comment.length <= 140));
+  });
+
+  it (`Has valid date`, () => {
+    assert(entitiy.hasOwnProperty(`date`));
+    const now = Date.now();
+    const week = 7 * 24 * 60 * 60 * 1000;
+    assert(entitiy.date >= now - week && entitiy.date <= now);
+  });
+});
